f9dc7aa49513e8904e1a096f64561eda250aba46
MarcinNiemczyk <m_niemczyk@outlook.com>
2026-05-23 16:25:56 +0200

    fix telescope

--- .config/nvim/lua/caqes/plugins/telescope.lua
+++ .config/nvim/lua/caqes/plugins/telescope.lua
@@ -10,6 +10,16 @@ return {
     config = function()
         require('telescope').setup({})
 
+        local preview_utils = require("telescope.previewers.utils")
+        preview_utils.ts_highlighter = function(bufnr, ft)
+            local lang = vim.treesitter.language.get_lang(ft) or ft
+            if not lang or lang == "" then
+                return false
+            end
+
+            return pcall(vim.treesitter.start, bufnr, lang)
+        end
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<leader>ps', function()
