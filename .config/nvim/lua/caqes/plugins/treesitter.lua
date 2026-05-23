7498c01b57f7e22b552dbb65b921a724f129b33d
MarcinNiemczyk <m_niemczyk@outlook.com>
2026-05-23 16:25:43 +0200

    migrate treesitter to the main branch

--- .config/nvim/lua/caqes/plugins/treesitter.lua
+++ .config/nvim/lua/caqes/plugins/treesitter.lua
@@ -1,60 +1,61 @@
 return {
     {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {
-                "vimdoc", "javascript", "typescript", "lua", "jsdoc", "bash", "python",
-            },
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        lazy = false,
+        init = function()
+            local parsers = {
+                "bash",
+                "cpp",
+                "diff",
+                "gitcommit",
+                "html",
+                "ini",
+                "javascript",
+                "jsdoc",
+                "json",
+                "just",
+                "lua",
+                "markdown",
+                "python",
+                "query",
+                "requirements",
+                "scss",
+                "tmux",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            }
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = {
-                enable = true
-            },
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-                disable = function(lang, buf)
-                    if lang == "html" then
-                        print("disabled")
-                        return true
+            local group = vim.api.nvim_create_augroup("ThePrimeagenTreesitter", { clear = true })
+            vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+                group = group,
+                callback = function()
+                    if vim.bo.buftype ~= "" then
+                        return
                     end
 
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        vim.notify(
-                            "File larger than 100KB treesitter disabled for performance",
-                            vim.log.levels.WARN,
-                            {title = "Treesitter"}
-                        )
-                        return true
-                    end
+                    pcall(vim.treesitter.start, 0)
                 end,
+            })
 
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
-            },
-        })
-
-    end
+            vim.api.nvim_create_autocmd("User", {
+                group = group,
+                pattern = "VeryLazy",
+                once = true,
+                callback = function()
+                    require("nvim-treesitter").install(parsers)
+                end,
+            })
+        end,
     },
 
     {
         "nvim-treesitter/nvim-treesitter-context",
-        after = "nvim-treesitter",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
         config = function()
             require'treesitter-context'.setup{
                 enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
